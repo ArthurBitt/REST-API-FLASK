fe27984b7baab48f2407ce8cfb14e87926c29cdb
ArthurBitt <add2coffee@gmail.com>
2025-01-03 15:10:02 -0300

    feat: adicionando mongodb engine e configurando primeiro model

---
+++ models.py
@@ -0,0 +1,8 @@
+from app import mongodb
+
+class UserModel(mongodb.Document):
+    name = mongodb.StringField(required=True)
+    last_name = mongodb.StringField(required=True)
+    cpf = mongodb.StringField(required=True, unique=True)
+    email = mongodb.EmailField(required=True)
+    birth_date = mongodb.DateField(required=True)
